8bf9d1bfe11e3dcefb2c06983500c64160cd1b10
stasm <stasmen@mail.ru>
2024-09-16 21:56:49 +0300

    фиксы

--- SkillProfi.Application/CQRS/Posts/Queries/GetList/PostDto.cs
+++ SkillProfi.Application/CQRS/Posts/Queries/GetList/PostDto.cs
@@ -7,16 +7,16 @@ namespace SkillProfi.Application.CQRS.Posts.Queries.GetList;
 public sealed class PostDto : IMapWith<Post>
 {
 	public int Id { get; set; }
-	public DateTime CreationDate { get; set; }
+	public long CreationDate { get; set; }
 	public string Title { get; set; } = string.Empty;
-	public string ImageUrl { get; set; } = string.Empty;
+	public string? ImageUrl { get; set; }
 	public string Description { get; set; } = string.Empty;
 
 	public void Mapping(Profile profile)
 	{
 		profile.CreateMap<Post, PostDto>()
 			   .ForMember(postDto => postDto.Id, opt => opt.MapFrom(post => post.Id))
-			   .ForMember(postDto => postDto.CreationDate, opt => opt.MapFrom(post => post.CreationDate))
+			   .ForMember(postDto => postDto.CreationDate, opt => opt.MapFrom(post => new DateTimeOffset(post.CreationDate).ToUnixTimeMilliseconds()))
 			   .ForMember(postDto => postDto.Title, opt => opt.MapFrom(post => post.Title))
 			   .ForMember(postDto => postDto.ImageUrl, opt => opt.MapFrom(post => post.ImageUrl))
 			   .ForMember(postDto => postDto.Description, opt => opt.MapFrom(post => post.Description));
